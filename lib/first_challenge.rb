5a7e17013dd623db26185e3d8bfe22e6cf0b792a
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-23 16:07:13 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,9 @@ def first_challenge
   }
 
   #your code here
+  contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
+ 
+puts contacts 
 
 
   #remember to return your newly altered contacts hash!
